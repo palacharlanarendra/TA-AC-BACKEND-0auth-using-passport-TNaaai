93ea2df2b2cae1381ed66fde9ac1abaf1a5a1335
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-08-12 14:23:02 +0530

    assign-3

--- block-BNaaef/DEMO/routes/articles.js
+++ block-BNaaef/DEMO/routes/articles.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Blog = require('../models/blogs');
 var Comments = require('../models/comments');
+const isLoggedIn = require('../middleware/auth');
 /* GET users listing. */
-router.get('/new', function (req, res, next) {
+router.get('/new', isLoggedIn, function (req, res, next) {
   res.render('addArticle');
 });
-router.get('/', function (req, res, next) {
+router.get('/', isLoggedIn, function (req, res, next) {
   Blog.find({}, (err, articles) => {
     res.render('articles', { articles: articles });
   });
@@ -22,7 +23,7 @@ router.get('/', function (req, res, next) {
 //   });
 // });
 
-router.get('/:id', function (req, res, next) {
+router.get('/:id', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Blog.findById(id)
     .populate('comments')
@@ -32,28 +33,28 @@ router.get('/:id', function (req, res, next) {
     });
 });
 
-router.get('/:id/edit', function (req, res, next) {
+router.get('/:id/edit', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Blog.findById(id, (err, articles) => {
     if (err) return next(err);
     res.render('editArticle', { articles: articles });
   });
 });
-router.post('/', function (req, res, next) {
+router.post('/', isLoggedIn, function (req, res, next) {
   Blog.create(req.body, (err, createArticle) => {
     console.log(err, createArticle);
     if (err) return next(err);
     res.redirect('/articles');
   });
 });
-router.post('/:id', function (req, res, next) {
+router.post('/:id', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Blog.findByIdAndUpdate(id, req.body, (err, createArticle) => {
     if (err) return next(err);
     res.redirect('/articles/' + id);
   });
 });
-router.get('/:id/delete', function (req, res, next) {
+router.get('/:id/delete', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Blog.findByIdAndDelete(id, (err, createArticle) => {
     if (err) return next(err);
@@ -63,7 +64,7 @@ router.get('/:id/delete', function (req, res, next) {
     });
   });
 });
-router.post('/:id/comments', function (req, res, next) {
+router.post('/:id/comments', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   req.body.articleId = id;
   Comments.create(req.body, (err, comment) => {
@@ -78,7 +79,7 @@ router.post('/:id/comments', function (req, res, next) {
     );
   });
 });
-router.get('/:id/likes', (req, res) => {
+router.get('/:id/likes', isLoggedIn, (req, res) => {
   var id = req.params.id;
   Blog.findByIdAndUpdate(id, { $inc: { likes: 1 } }, (err, article) => {
     if (err) return next(err);
--- block-BNaaef/DEMO/routes/comments.js
+++ block-BNaaef/DEMO/routes/comments.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Comments = require('../models/comments');
 var Blog = require('../models/blogs');
-router.get('/:id/edit', function (req, res, next) {
+const isLoggedIn = require('../middleware/auth');
+router.get('/:id/edit', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Comments.findById(id, (err, comment) => {
     if (err) return next(err);
@@ -10,20 +11,24 @@ router.get('/:id/edit', function (req, res, next) {
   });
 });
 
-router.post('/:id', (req, res, next) => {
+router.post('/:id', isLoggedIn, (req, res, next) => {
   var id = req.params.id;
   Comments.findByIdAndUpdate(id, req.body, (err, updatedComments) => {
     if (err) return next(err);
     res.redirect('/articles/' + updatedComments.articleId);
   });
 });
-router.get('/:id/delete', function (req, res, next) {
+router.get('/:id/delete', isLoggedIn, function (req, res, next) {
   var id = req.params.id;
   Comments.findByIdAndDelete(id, (err, comment) => {
     if (err) return next(err);
-    Blog.findByIdAndUpdate(comment.bookId,{$pull:{comments:comment._id}},(err,blog)=>{
+    Blog.findByIdAndUpdate(
+      comment.bookId,
+      { $pull: { comments: comment._id } },
+      (err, blog) => {
         res.redirect('/articles/' + comment.articleId);
-    })
+      }
+    );
   });
 });
 module.exports = router;
